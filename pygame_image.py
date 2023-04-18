b6f373ffdd80cc83691dd1b754f23deaf1c5e70a
kota <c0b2204854@edu.teu.ac.jp>
2023-04-18 16:15:42 +0900

    練習５

--- pygame_image.py
+++ pygame_image.py
@@ -8,8 +8,8 @@ def main():
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
     kk_img01 = pg.image.load("ex01/fig/3.png")
     kk_img01 = pg.transform.flip(kk_img01,True,False)
-    kk_ing02 = pg.transform.rotozoom(kk_img01,10,1.0)
-    kk_imgs = [kk_img01,kk_ing02]
+    kk_img02 = pg.transform.rotozoom(kk_img01,10,1.0)
+    kk_imgs = [kk_img01,kk_img02]
 
     tmr = 0
 
@@ -19,9 +19,16 @@ def main():
         
         tmr += 1
         screen.blit(bg_img, [0, 0])
+        if tmr%2 == 0:
+            screen.blit(kk_imgs[0],[300,200])
+        else:
+            screen.blit(kk_imgs[1],[300,200])
+
         #screen.blit(kk_img,[300,200])
         pg.display.update()
+
         clock.tick(100)
+        print(tmr)
 
 
 if __name__ == "__main__":
